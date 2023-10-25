b6df2eb56d14696221811e4f9f7c16189e331fce
vegardinho <landsverk.vegard@gmail.com>
2023-10-26 00:52:06 +0200

    Bugfixes

--- custom_scripts/add_tag_new.py
+++ custom_scripts/add_tag_new.py
@@ -15,10 +15,10 @@ def main():
     root = sys.argv[1].replace(" ", "\\ ")
     mode = sys.argv[2]
     tags = sys.argv[3:]
+    output = ['']
 
     if sys.argv[2][0] != '-':
-        exit_code = -10
-        sys.exit(exit_code)
+        sys.exit(10)
 
     f = open(f"{os.environ['FISH_DIR']}/conf.d/dynamic_abbr.fish", "r+")
     text = f.read()
@@ -26,6 +26,8 @@ def main():
     if mode in ['-a', '-add']:
        for tag in tags:
         # Match root path if tag in line
+        if tag[0] == '-':
+            sys.exit(10)
         match = re.search(rf'(?<={re.escape(tag)}\s).*(?!=abbr)', text)
         if match:
             # Replace matched root with supplemented root
@@ -37,18 +39,23 @@ def main():
                     line_end = ''
                 # Ensure line break before and after new line
             text += "{}abbr -a -g {} \"{}\"\n".format(line_end, tag, root)
+        output = tags
+
     elif mode in ['-d', '-delete']:
         # Delete all occurences of directory
         tag_hit = ''
-        if len(tags) == 1:
+        output = tags
+        if tags[-1][0] == '-':
+            if tags[-1] != '--all':
+                sys.exit(10)
+            output = ['[all tags]']
+        elif len(tags) == 1:
             tag_hit = tags[0]
         elif len(tags) > 1:
             tag_hit = rf'({"|".join(tags)})'
-        if sys.argv[-1][0] == '-':
-            if sys.argv[-1] != '--all':
-                sys.exit(10)
+            output = tags
         else:
-            exit_code = 10
+            sys.exit(20)
         text = re.sub(rf'abbr.*{tag_hit}\s\"{re.escape(root)}\"\n?(?!=abbr)', '', text)
         exit_code = 50
         # TODO: list what is deleted
@@ -56,14 +63,16 @@ def main():
         groups = re.findall(rf'(?<=\s)\S+(?=\s"{re.escape(root)})', text)
         output = ['[None]'] if len(groups) == 0 else groups
 
-        # Write list of tags to tmp file
-        path = os.path.join('/tmp', "fish_tags_current.txt")
-        with open(path, 'w') as fp:
-            fp.write(', '.join(output))
         exit_code = 20
     else:
         exit_code = 10
         sys.exit(10)
+
+    # Write list of tags to tmp file
+    path = os.path.join('/tmp', "fish_tags.txt")
+    with open(path, 'w') as fp:
+        output = output[0] if len(tags) == 1 else ' '.join(output)
+        fp.write(output)
     
     f.seek(0)
     f.truncate(0)
