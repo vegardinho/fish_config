46701282a06aab1b484010c332487b09d2967754
vegardinho <landsverk.vegard@gmail.com>
2025-03-20 21:46:29 +0100

    First check if workspace file

--- functions/code.fish
+++ functions/code.fish
@@ -1,3 +1,8 @@
-function code $argv
-   open "$argv" -a "Visual Studio Code"
+function code
+    set workspace_file (find . -maxdepth 1 -name '*.code-workspace' 2>/dev/null)
+    if test -n "$workspace_file"
+        open "$workspace_file" -a "Visual Studio Code"
+    else
+        open "$argv" -a "Visual Studio Code"
+    end
 end
